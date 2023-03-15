8d2ff905d3921ce9634c8846d566ea1c964c4bd1
Givaldo Neto <valdingivs@outlook.com>
2023-03-15 16:49:45 -0300

    :sparkles: feat: add default colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,12 @@
 module.exports = {
   content: ['./src/**/*.jsx'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#7749F8',
+        primary_dark: '#5227CC',
+        primary_light: '#EBE5FC'
+      }
+    }
+  }
 };
