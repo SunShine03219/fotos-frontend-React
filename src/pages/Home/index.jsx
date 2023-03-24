b07c1c9938178cdd6e71b61111772935b62a09f6
Givaldo Neto <valdingivs@outlook.com>
2023-03-24 01:34:07 -0300

    feat: using new components

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,18 @@
 import React, {useEffect, useState} from 'react'
 
-import { Header } from '../../components/Header'
+import { Header } from '../../components/UI/Header'
 import { Tabs } from '../../components/Tabs'
 import { getUserData } from "../../services/api";
 
+import { useUsers } from "../../context/usersContext";
 
 export function HomePage(){
     const [userName, setUsername] = useState(null)
 
     useEffect( () => {
         async function fetchName(){
-            const name = await getUserData()
-            setUsername(name)
+            const data = await getUserData()
+            setUsername(data.name)
         }
         fetchName()
     }, [])
