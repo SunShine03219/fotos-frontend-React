b6a616d864a580fe9dd0ed71e19075e6eead5081
Givaldo Neto <valdingivs@outlook.com>
2023-03-24 11:50:14 -0300

    feat: set users context only if users is logged in

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,13 +4,20 @@ import { useAuth } from "../hooks/auth";
 import { AppRoutes } from "./app.routes"
 import { AuthRoutes } from "./auth.routes"
 
+import { UserProvider } from "../context/usersContext";
+
 export function Routes(){
 
     const { user } = useAuth();
 
     return (
         <BrowserRouter>
-            {user ? <AppRoutes/> : <AuthRoutes/>}
+            {user ?
+                <UserProvider>
+                    <AppRoutes/>
+                </UserProvider>
+                :
+                <AuthRoutes/>}
         </BrowserRouter>
     )
 }
