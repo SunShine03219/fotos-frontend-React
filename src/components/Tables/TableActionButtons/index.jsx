478ed9e2003b935e40062b5a5f6d3880f688a071
Givaldo Neto <valdingivs@outlook.com>
2023-03-31 19:03:39 -0300

    :sparkles: remove delete from main folders

--- src/components/Tables/TableActionButtons/index.jsx
+++ src/components/Tables/TableActionButtons/index.jsx
@@ -1,6 +1,10 @@
 import {AiOutlineCloudDownload, AiOutlineDelete, AiOutlineEdit} from "react-icons/ai";
 
 export function TableActionButtons({ rowTitle, item, onEditClick, onDeleteClick, onDownloadClick }) {
+
+    const shouldShowDeleteButton =
+        item.title !== "fotos-originais" && item.title !== "fotos-otimizadas";
+
     return (
         <td className="py-2 px-2">
             <div className="inline-flex space-x-2"> {/* or you can use "space-x-2" */}
@@ -17,11 +21,13 @@ export function TableActionButtons({ rowTitle, item, onEditClick, onDeleteClick,
                         onClick={() => onEditClick(item.name)}
                     />
                 )}
-                <AiOutlineDelete
-                    size="1.5em"
-                    className="cursor-pointer hover:text-red-500"
-                    onClick={() => onDeleteClick(item.title ? item.title : item.name)}
-                />
+                {shouldShowDeleteButton && (
+                    <AiOutlineDelete
+                        size="1.5em"
+                        className="cursor-pointer hover:text-red-500"
+                        onClick={() => onDeleteClick(item.title ? item.title : item.name)}
+                    />
+                )}
             </div>
         </td>
     )
