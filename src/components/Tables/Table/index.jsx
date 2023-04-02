e9d69a9e79fc12efbba8584d0d9959e744465395
loliveirawebdev <loliveirawebdev@gmail.com>
2023-04-02 10:46:27 -0300

    feat: sort folders and pictures

--- src/components/Tables/Table/index.jsx
+++ src/components/Tables/Table/index.jsx
@@ -1,64 +1,67 @@
-import React, {useState} from 'react'
-import { PictureRow } from '../TableRow'
-import { useAlertModal } from "../../../hooks/useAlertModal"
-import { AlertModal } from "../../Modals/AlertModal"
+import React, { useMemo } from "react";
+import { PictureRow } from "../TableRow";
+import { useAlertModal } from "../../../hooks/useAlertModal";
+import { AlertModal } from "../../Modals/AlertModal";
+import { hasFileExtension } from "../../../utils/helpers";
 
+export function Table({ data, action, onFolderClickTable, tableName }) {
+  const { showModal, setShowModal, setMessage, setType, message, type } = useAlertModal();
 
-    export function Table({ data, action, onFolderClickTable, tableName, currentPage }) {
+  const sortedData = useMemo(() => {
+    if (tableName === "Users") return data;
 
-        const {
-            showModal,
-            setShowModal,
-            setMessage,
-            setType,
-            message,
-            type,
-        } = useAlertModal()
+    const sortedData = data.sort((a, b) => {
+      const aHasExtension = hasFileExtension(a.title);
+      const bHasExtension = hasFileExtension(b.title);
 
+      if (aHasExtension && !bHasExtension) {
+        return 1;
+      } else if (!aHasExtension && bHasExtension) {
+        return -1;
+      } else {
+        return a.title.localeCompare(b.title);
+      }
+    });
 
-        const handleAlert = (message, type) => {
-            setMessage(message)
-            setType(type)
-            setShowModal(true)
-            setTimeout(() => setShowModal(false), 1500)
-        }
+    return sortedData;
+  }, [data]);
 
-    const handleFolderClick = async (item) => {
-        onFolderClickTable(item)
-    }
+  const handleAlert = (message, type) => {
+    setMessage(message);
+    setType(type);
+    setShowModal(true);
+    setTimeout(() => setShowModal(false), 1500);
+  };
 
-    return (
-        <>
-            <table className="table-fixed w-full">
-                <thead>
-                <tr>
-                    <th className="w-[90%] py-2"></th>
-                    <th className="w-[5%] py-2"></th>
-                    <th className="w-[5%] py-2"></th>
-                </tr>
-                </thead>
-                <tbody>
-                {data.map((item, index) => (
-                    <PictureRow
-                        key={item.title ? item.title : item.id}
-                        item={item}
-                        index={index}
-                        currentPage={currentPage}
-                        rowTitle={action}
-                        onFolderClick={handleFolderClick}
-                        tableName={tableName}
-                        onAlert={handleAlert}
-                    />
-                ))}
-                </tbody>
-            </table>
-            {showModal && (
-                <AlertModal
-                    message={message}
-                    type={type}
-                />
-            )}
-        </>
+  const handleFolderClick = async (item) => {
+    onFolderClickTable(item);
+  };
 
-    )
+  return (
+    <>
+      <table className="table-fixed w-full">
+        <thead>
+          <tr>
+            <th className="w-[90%] py-2"></th>
+            <th className="w-[5%] py-2"></th>
+            <th className="w-[5%] py-2"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedData.map((item, index) => (
+            <PictureRow
+              key={item.title ? item.title : item.id}
+              item={item}
+              index={index}
+              rowTitle={action}
+              onFolderClick={handleFolderClick}
+              tableName={tableName}
+              onAlert={handleAlert}
+            />
+          ))}
+        </tbody>
+      </table>
+      {showModal && <AlertModal message={message} type={type} />}
+    </>
+  );
 }
